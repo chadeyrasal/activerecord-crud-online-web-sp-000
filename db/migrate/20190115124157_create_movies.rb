7cd3b07236d8f4c07ac471b7ba5684169afb080b
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-15 12:45:37 +0000

    Automatically backed up by Learn

--- db/migrate/20190115124157_create_movies.rb
+++ db/migrate/20190115124157_create_movies.rb
@@ -1,4 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
+    create_table :movies do |t|
+      t.string
+      t.integer
+      t.string
+      t.string
+      t.boolean
   end
 end
