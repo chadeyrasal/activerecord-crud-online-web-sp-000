dffdb1e99ba1eeb9aa2de1da6dc83e61f1d73d5e
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-15 13:14:40 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -35,7 +35,7 @@ def can_be_created_in_a_block(args = __)
 end
 
 def can_get_the_first_item_in_the_database
-  movie = Movie.find(1)
+  movie = Movie.first
 end
 
 def can_get_the_last_item_in_the_database
@@ -47,7 +47,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  movie = Movie.find(1)
 end
 
 def can_find_by_multiple_attributes
