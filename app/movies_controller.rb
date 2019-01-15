db73e0e0c61362243f1a37f853f6506556b3c77c
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-01-15 14:43:33 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -75,7 +75,7 @@ def can_update_multiple_items_at_once
   Movie.all.each {|movie| movie.update(title: 'A Movie')}
 #    movie.title = 'A Movie'
 #    movie.save
-  end
+#  end
 end
 
 def can_destroy_a_single_item
